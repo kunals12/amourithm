4e177a38bbbdaa05c8c628a68759230f96cfdbb3
kunals12 <work.kunal12@gmail.com>
2024-12-03 21:37:25 +0530

    server setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/", web::get().to(hello_world)))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
+
+async fn hello_world() -> impl Responder {
+    HttpResponse::Ok().body("Hello World")
 }
